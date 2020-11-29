5ddd34edb45a6ae4779c69a2fb03d157676b266f
Hageru Ray <43041149+RayhanHagel@users.noreply.github.com>
2020-11-29 22:27:43 +0700

    Create Setup File

---
+++ discord-bot.py
@@ -0,0 +1,17 @@
+import os
+from discord.ext import commands
+
+
+TOKEN = '{Insert_Token_Here}'
+
+client = commands.Bot(command_prefix='.', help_command=None)
+os.system('cls')
+
+for filename in os.listdir(r'./commands'):
+    if filename.endswith('.py'):
+        try:
+            client.load_extension(f'commands.{filename[:-3]}')
+        except Exception:
+            print(f'Module {filename} is having an error....')
+
+client.run(TOKEN)
