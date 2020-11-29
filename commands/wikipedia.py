f74c18f7271e0bc43680464654ad24d77bce8b95
Hageru Ray <43041149+RayhanHagel@users.noreply.github.com>
2020-11-29 22:36:27 +0700

    Wikipedia Search
    Gets the first paragraph from an article in Wikipedia

---
+++ commands/wikipedia.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import discord
+import datetime
+import wikipediaapi
+
+
+class Wikipedia(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+ 
+    @commands.command()
+    async def wiki(self, ctx, *, search):
+        wiki = wikipediaapi.Wikipedia('en')
+        page = wiki.page(search)
+        dot = page.summary.find('\n')
+        
+        embed = discord.Embed(title=page.title.capitalize(),
+                              url=page.fullurl,
+                              description=page.summary[0:dot].replace(page.title, f'**{page.title}**').replace(page.title.lower(), f'**{page.title.lower()}**'),
+                              timestamp=datetime.datetime.now(datetime.timezone.utc))
+        embed.set_footer(text=f'Wikipedia Article')
+        await ctx.send(embed=embed)
+        
+        
+def setup(client):
+    client.add_cog(Wikipedia(client))
