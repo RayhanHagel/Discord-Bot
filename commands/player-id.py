c8856db68f99a17796f57f81f28304ba6259d7d6
Hageru Ray <43041149+RayhanHagel@users.noreply.github.com>
2020-11-29 22:50:54 +0700

    Gets @mentioned player's Discord ID

---
+++ commands/player-id.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import discord
+import datetime
+
+
+class Player_ID(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def id(self, ctx, args=None):
+        if args is None:
+            embed = discord.Embed(title=f':zap: {ctx.author.name} \n:sparkles: {ctx.author.id} \n',
+                                  timestamp=datetime.datetime.now(datetime.timezone.utc))
+            await ctx.send(embed=embed, delete_after=10)
+        elif args.find("<@!") != -1:
+            embed = discord.Embed(title=f':sparkles: {args.replace("<@!", "").replace(">", "")} \n',
+                                  timestamp=datetime.datetime.now(datetime.timezone.utc))
+            await ctx.send(embed=embed)
+        else:
+            pass
+
+
+def setup(client):
+    client.add_cog(Player_ID(client))
+
+# Hageru-Ray
